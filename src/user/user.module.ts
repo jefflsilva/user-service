c82bb5129dfc7edd8f2bdd64f94e6f1b50d8c854
Jefferson <jeffersondevlsilva@gmail.com>
2025-03-23 01:48:14 -0300

    feat: configure UserModule with dependency injection

---
+++ src/user/user.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './application/user.service';
+import { PrismaUserRepository } from './infrastructure/prisma.user.repository';
+import { BcryptHashUtil } from './infrastructure/bcrypt.hash.util';
+import { PrismaService } from '../config/prisma.service';
+
+@Module({
+  controllers: [UserController],
+  providers: [
+    UserService,
+    PrismaService,
+    { provide: 'IUserRepository', useClass: PrismaUserRepository },
+    { provide: 'IHashUtil', useClass: BcryptHashUtil },
+  ],
+})
+export class UserModule {}
